caccfd095bf755b210874e8d1a4f04f4cc160199
Martialis39 <mart_lepanen@hotmail.com>
2020-11-05 09:10:58 +0200

    Wipe the slate the clean

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
-import { map, take, flatMap, tap, withLatestFrom, scan, filter } from 'rxjs/operators'
-import {input, chooseSide, board, winStates, testStream, anotherTestStream} from './observables'
-import {getTarget, getAttributeFromDataset, checkForWin} from './helpers'
+// import {} from 'rxjs/operators'
+// import {} from './observables'
+// import {} from './helpers'
 
-const inputStream = input
-  .pipe(
-    map(getTarget),
-    map(getAttributeFromDataset('index'))
-  )
-
-winStates.subscribe(console.log.bind(null, 'Got this: '))
-
-const chooseSideStream = chooseSide
-  .pipe(
-      map(getTarget),
-      map(getAttributeFromDataset('side')),
-      take(1)
-    )
-
-const gameStream = inputStream
-  .pipe(
-    withLatestFrom(chooseSideStream),
-    withLatestFrom(board),
-    map(([[index, side], board]) => ({index, side, board})),
-    scan((oldState, newState, iteration) => {
-      const {index, side} = newState;
-      const board = iteration === 0 ? newState.board : oldState.board;
-      board[index] = side;
-      return {board}
-    }, {}),
-    withLatestFrom(winStates),
-    map(([board, winStates]) => checkForWin(board, winStates))
-  )
-
-gameStream.subscribe(console.log.bind(null, 'gameStream Got this: '))
